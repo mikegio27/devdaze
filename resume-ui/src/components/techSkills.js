0fdbc893bc94243fdc2da4fdd3b8e32b60746ae8
Mike Giovanetti <eth3real27@gmail.com>
2023-10-09 16:49:15 -0700

    update API endpoint to api service

--- resume-ui/src/components/techSkills.js
+++ resume-ui/src/components/techSkills.js
@@ -4,7 +4,7 @@ const TechSkills = () => {
     const [techSkillsData, setTechSkillsData] = useState();
 
     useEffect(() => {
-        fetch("http://localhost:8080/resume/techskills")
+        fetch("http://api-service.web-apps.svc.cluster.local/resume/techskills")
             .then(response => response.json())
             .then(data => {
                 console.log("DATA: ", JSON.stringify(data))
