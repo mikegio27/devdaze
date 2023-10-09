2b4e84356d96d16114e85b4ed0f77c507eaa8f2b
Mike Giovanetti <eth3real27@gmail.com>
2023-10-08 22:46:37 -0700

    deployment setup

--- src/main.go
+++ src/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/mikegio27/resume-api/database"
 	v1 "github.com/mikegio27/resume-api/v1"
 )
 
 func main() {
-	database.ConnectMongoDB("mongodb://admin:password@localhost:27017/")
+	username := os.Getenv("MONGO_USERNAME")
+	pass := os.Getenv("MONGO_PASSWORD")
+	uri := fmt.Sprintf("mongodb://%s:%s@localhost:27017/", username, pass)
+	database.ConnectMongoDB(uri)
 	r := v1.SetupRouter()
 
 	r.Run(":8080")
