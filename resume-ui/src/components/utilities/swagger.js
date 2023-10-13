83a3328df254dce4296520a22e69685fe9e67d18
Mike Giovanetti <eth3real27@gmail.com>
2023-10-13 15:52:19 -0700

    increase height of iframe

--- resume-ui/src/components/utilities/swagger.js
+++ resume-ui/src/components/utilities/swagger.js
@@ -3,7 +3,7 @@ const Swagger = () => {
         <iframe 
             title="Swagger Documentation"
             width="100%" 
-            height="600px" 
+            height="80%px" 
             src="https://api.devdaze.org/swagger/index.html">
         </iframe>
     );
